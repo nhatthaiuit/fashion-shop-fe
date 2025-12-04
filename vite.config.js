07f6412d7f7500d3a7c9ada241e4ace8b8dfa7b3
nhatthaiuit <nhatthaidx@gmail.com>
2025-12-04 13:36:08 +0700

    Cleanup gitignore and add deployment url to dev server logs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'custom-logger',
+        configureServer(server) {
+          const _printUrls = server.printUrls
+          server.printUrls = () => {
+            _printUrls()
+            const deployUrl = env.VITE_DEPLOY_URL || "https://fashion-shop-frontend.vercel.app" // Fallback or empty
+
+            console.log(`  ➜  🚀 LOCAL:      http://localhost:${server.config.server.port || 5173}/`)
+            if (deployUrl) {
+              console.log(`  ➜  🌍 DEPLOYMENT: ${deployUrl}/`)
+            }
+          }
+        }
+      }
+    ],
+  }
 })
